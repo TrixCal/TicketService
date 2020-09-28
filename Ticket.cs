d1f8dcd601018098de40bf648d4cb7a697ea1a74
TrixCal <traytonlarson01@gmail.com>
2020-09-28 11:09:54 -0500

    Display Ticket

--- Ticket.cs
+++ Ticket.cs
@@ -11,5 +11,10 @@ namespace TicketService
         public string submitter { get; set; }
         public string assigned { get; set; }
         public List<string> watching { get; set; }
+
+        public string Display()
+        {
+            return $"{ticketID}) {status}\nStatus) {priority}\nPriority) {submitter}\nSubmitter) {assigned}\nAssigned) {string.Join(", ", watching)}\nWatching) {6}";
+        }
     }
 }
