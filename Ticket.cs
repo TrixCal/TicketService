c4e71bc20de97a48998adc637112bf874e494cb9
TrixCal <traytonlarson01@gmail.com>
2020-11-15 14:32:43 -0600

    Added new ticket types

--- Ticket.cs
+++ Ticket.cs
@@ -13,18 +13,37 @@ namespace TicketService
         public string assigned { get; set; }
         public List<string> watching { get; set; }
 
-        public Ticket(string[] ticketArray)
-        {
-            summary = ticketArray[0];
-            status = ticketArray[1];
-            priority = ticketArray[2];
-            submitter = ticketArray[3];
-            assigned = ticketArray [4];
-            watching = new List<string>(ticketArray[5].Split("|"));
-        }
-        public string Display()
+        public virtual string Display()
         {
             return $"Ticket {ticketID} \nSummary) {summary} \nStatus) {status}\nPriority) {priority}\nSubmitter) {submitter}\nAssigned) {assigned}\nWatching) {string.Join(", ", watching)}";
+        }
+    }
+    public class Bug : Ticket
+    {
+        public string severity { get; set; }
+        public override string Display()
+        {
+            return $"Ticket {ticketID} \nSummary) {summary} \nStatus) {status}\nPriority) {priority}\nSubmitter) {submitter}\nAssigned) {assigned}\nWatching) {string.Join(", ", watching)}\nSeverity) {severity}";
+        }
+    }
+    public class Enhancement : Ticket
+    {
+        public string software { get; set; }
+        public double cost { get; set; }
+        public string reason { get; set; }
+        public double estimate { get; set; }
+        public override string Display()
+        {
+            return $"Ticket {ticketID} \nSummary) {summary} \nStatus) {status}\nPriority) {priority}\nSubmitter) {submitter}\nAssigned) {assigned}\nWatching) {string.Join(", ", watching)}\nSoftware) {software}\nCost) {cost:C}\nReason) {reason}\nEstimate) {estimate:C}";
+        }
+    }
+    public class Task : Ticket
+    {
+        public string projectName { get; set; }
+        public DateTime duedate { get; set; }
+        public override string Display()
+        {
+            return $"Ticket {ticketID} \nSummary) {summary} \nStatus) {status}\nPriority) {priority}\nSubmitter) {submitter}\nAssigned) {assigned}\nWatching) {string.Join(", ", watching)}\nProject Name) {projectName}\nDue Date) {duedate:d}";
         }
     }
 }
